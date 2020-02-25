2adabef257f434b093bd457069ca65cd06f7db01
Hira <hiraahsan94@gmail.com>
2020-02-25 10:30:57 -0500

    setting up webpack configuration for js/sass/html

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
     module: {
         rules: [
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+
+                // loads jquery
+            },
             {
                 test: /\.html$/,
                 use: [
@@ -10,7 +22,27 @@ module.exports = {
                         loader: "html-loader",
                         options: { minimize: true }
                     }
+
+                    // loads the html
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+
+                // Graphics support for images etc.
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+
+                // sass support
             }
         ]
     },
@@ -19,5 +51,13 @@ module.exports = {
             template: "./src/index.html",
             filename: "./index.html"
         }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+
+        // could have done away with using the css plugin. what it does it, it will run during production build
     ]
 }
