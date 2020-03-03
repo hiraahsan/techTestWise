81a0ea05229e37364ebc53356aeb714e122c88de
Hira <hiraahsan94@gmail.com>
2020-03-03 01:10:12 -0500

    cleaning extraneous code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ app.lastName = document.getElementById("lastName");
 app.email = document.getElementById("email");
 app.symbol = "";
 
-let stocks = document.getElementById("stockTicker");
 let msft = document.getElementById("microsoftStocks");
 let btc = document.getElementById("bitcoinStocks");
 let google = document.getElementById("googleStocks");
@@ -40,19 +39,17 @@ document.getElementById("subscribe").addEventListener("submit", (event) => {
     })
     .then( (result) => {
         app.lastRefreshed = result['Meta Data']['3. Last Refreshed']
-        // console.log(app.lastRefreshed)
         const regex = /\d{2}:\d{2}:\d{2}/g;
         const replace = ``;
         const updatedDate = app.lastRefreshed.replace(regex, replace)
         let newUpdatedDate = updatedDate.replace(/\s/g, '')
-        // console.log(`regexed it`, updatedDate)
         console.log(newUpdatedDate)
         console.log(result['Time Series (Daily)']);
         app.symbol = result['Meta Data']['2. Symbol'];
         app.closingPrice = result['Time Series (Daily)'][newUpdatedDate]['4. close']
         app.openingPrice = result['Time Series (Daily)'][newUpdatedDate]['1. open']
 
-        msft.append(`${app.symbol}, ${app.closingPrice}, ${app.openingPrice}, ${app.lastRefreshed}`)
+        msft.append(`${app.symbol} ${app.closingPrice}`)
         if (app.closingPrice - app.openingPrice >= 0) {
             msftStatus.append("↑")
             $("#msftStatus").css("color", "green");
@@ -76,19 +73,17 @@ const callApiGoogle = () => {
     })
     .then( (result) => {
         app.lastRefreshed = result['Meta Data']['3. Last Refreshed']
-        // console.log(app.lastRefreshed)
         const regex = /\d{2}:\d{2}:\d{2}/g;
         const replace = ``;
         const updatedDate = app.lastRefreshed.replace(regex, replace)
         let newUpdatedDate = updatedDate.replace(/\s/g, '')
-        // console.log(`regexed it`, updatedDate)
         console.log(newUpdatedDate)
         console.log(result['Time Series (Daily)']);
         app.symbol = result['Meta Data']['2. Symbol'];
         app.closingPrice = result['Time Series (Daily)'][newUpdatedDate]['4. close']
         app.openingPrice = result['Time Series (Daily)'][newUpdatedDate]['1. open']
 
-        google.append(`${app.symbol}, ${app.closingPrice}, ${app.openingPrice}, ${app.lastRefreshed}`)
+        google.append(`${app.symbol} ${app.closingPrice}`)
 
         if (app.closingPrice - app.openingPrice >= 0) {
             googleStatus.append("↑")
@@ -119,14 +114,13 @@ const callApiBitcoin = () => {
         const replace = ``;
         const updatedDate = app.lastRefreshed.replace(regex, replace)
         let newUpdatedDate = updatedDate.replace(/\s/g, '')
-        // console.log(`regexed it`, updatedDate)
         console.log(newUpdatedDate)
         console.log(result['Time Series (Daily)']);
         app.symbol = result['Meta Data']['2. Symbol'];
         app.closingPrice = result['Time Series (Daily)'][app.lastRefreshed]['4. close']
         app.openingPrice = result['Time Series (Daily)'][app.lastRefreshed]['1. open']
 
-        btc.append(`${app.symbol}, ${app.closingPrice}, ${app.openingPrice}, ${app.lastRefreshed}`)
+        btc.append(`${app.symbol} ${app.closingPrice}`)
 
         if (app.closingPrice - app.openingPrice >= 0) {
             bitcoinStatus.append("↑")
@@ -147,3 +141,9 @@ callApiBitcoin();
 callApiGoogle()
 
 callApi();
+
+$('form').submit( (e) => {
+    e.preventDefault();
+    let formData = $(this).serializeArray();
+    $.get('php/echo-input-fields.php', formData)
+  });
